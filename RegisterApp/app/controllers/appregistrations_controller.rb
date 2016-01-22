6af825d5e308baf32d0176bcf4a1f1bffb46b41a
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-01-22 17:15:29 +0100

    Appreg Controller updated

--- RegisterApp/app/controllers/appregistrations_controller.rb
+++ RegisterApp/app/controllers/appregistrations_controller.rb
@@ -1,2 +1,34 @@
 class AppregistrationsController < ApplicationController
+
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @appregistration = current_user.appregistrations.build(appregistration_params)
+    if @appregistration.save
+      flash[:success] = "Appregistration created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @appregistration.destroy
+    flash[:success] = "Appregistration deleted"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+  def appregistration_params
+    params.require(:appregistration).permit(:content)
+  end
+
+  def correct_user
+    @appregistration = current_user.appregistrations.find_by(id: params[:id])
+    redirect_to root_url if @appregistration.nil?
+  end
+
 end
