dd8f34e9eef915a25a047b3eeec825266dbe13e6
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-01-22 17:11:49 +0100

    ApplicationController updated

--- RegisterApp/config/routes.rb
+++ RegisterApp/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
-  get 'static_pages/home'
+  root                'static_pages#home'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
+  resources :appregistrations, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
