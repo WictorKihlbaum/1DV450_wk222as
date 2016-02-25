c1c0742588a342b67b6f070f00363d31d9c46447
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-02-25 17:13:36 +0100

    WebAPI created.
    WebAPI created with ’rails-api’-gem. Views, assets and such are not
    needed for this.

---
+++ WebAPI/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+WebAPI::Application.config.secret_token = 'd4703993834b45feea4a8143456a97301adf443780c6b24014e20b7a0f388948149b35499431ca75bc4f377257088d7171329b345adc7be1af1832a19574cb41'
