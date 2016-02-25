2859c92dbaf9dd8cbcf56944197be5b7c5058d9a
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-02-25 17:31:30 +0100

    event show updated.
    User can now find resource by ID, and retrieve it in json or xml.

--- WebAPI/app/controllers/events_controller.rb
+++ WebAPI/app/controllers/events_controller.rb
@@ -2,20 +2,23 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :update, :destroy]
 
   # GET /events
-  # GET /events.json
   def index
     @events = Event.all
 
     respond_to do |format|
-      format.json { render json: @events }
-      format.xml { render xml: @events }
+      format.json { render json: @events, status: :ok }
+      format.xml { render xml: @events, status: :ok }
     end
   end
 
-  # GET /events/1
-  # GET /events/1.json
+  # GET /events/id
   def show
-    render json: @event
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: @event, status: :ok }
+      format.xml { render xml: @event, status: :ok }
+    end
   end
 
   # POST /events
