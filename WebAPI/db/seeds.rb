129d9159f5c3ab8286c1515a9764cc67a1f6a63e
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-02-26 11:05:31 +0100

    DB seeded

--- WebAPI/db/seeds.rb
+++ WebAPI/db/seeds.rb
@@ -7,14 +7,32 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-=begin
+Position.create!(
+    longitude: 16.3666667,
+    latitude: 56.6666667
+)
+
+Tag.create!(
+    name: 'Paintball'
+)
+
+Creator.create!(
+    first_name: 'Wictor',
+    last_name: 'Kihlbaum'
+)
+
 Event.create!(
     category: "Paintball",
-    description: 'Test your skills against your friends in Kalmar paintball-arena!'
+    description: 'Test your skills against your friends in Kalmar paintball-arena!',
+    creator_id: 1,
+    position_id: 1,
+    tag_id: 1
 )
 
 Event.create!(
     category: "Winetasting",
-    description: 'Taste all our wines for a cheap price!'
+    description: 'Taste all our wines for a cheap price!',
+    creator_id: 1,
+    position_id: 1,
+    tag_id: 1
 )
-=end
