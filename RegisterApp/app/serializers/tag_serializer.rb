dc4215eca7a0ca9d943b73f80ba82bf98dcc3af9
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-04-13 19:20:53 +0200

    Tag Serializer created and updated

---
+++ RegisterApp/app/serializers/tag_serializer.rb
@@ -0,0 +1,12 @@
+class TagSerializer < ActiveModel::Serializer
+
+  attributes :id, :name
+
+  def links
+    {
+        self: api_tag_path(object.id),
+        events: api_tag_events_path(object.id)
+    }
+  end
+
+end
