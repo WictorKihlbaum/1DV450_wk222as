82108905da5ddb60371dbb6a7cdae34fa6a553ca
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-04-13 08:47:19 +0200

    Controller methods for missing controllers have been added
    index, show, set

--- RegisterApp/app/controllers/api/v1/creators_controller.rb
+++ RegisterApp/app/controllers/api/v1/creators_controller.rb
@@ -4,11 +4,16 @@ class API::V1::CreatorsController < API::APIController
 
 
   def index
-
+    creators = Creator.limit(@limit).offset(@offset)
+    render_response(creators, :ok)
   end
 
   def show
+    render_response(@creator, :ok)
+  end
 
+  def set_creator
+    @creator = Creator.find(params[:id])
   end
 
 end
--- RegisterApp/app/controllers/api/v1/tags_controller.rb
+++ RegisterApp/app/controllers/api/v1/tags_controller.rb
@@ -4,11 +4,16 @@ class API::V1::TagsController < API::APIController
 
 
   def index
-
+    tags = Tag.limit(@limit).offset(@offset)
+    render_response(tags, :ok)
   end
 
   def show
+    render_response(@tag, :ok)
+  end
 
+  def set_tag
+    @tag = Tag.find(params[:id])
   end
 
 end
