adde59df81aa4b6c29b916f92cbd748b66040fb2
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-04-13 18:52:19 +0200

    Serializer for Event implemented

---
+++ RegisterApp/app/serializers/event_serializer.rb
@@ -0,0 +1,18 @@
+class EventSerializer < ActiveModel::Serializer
+  attributes :id, :category, :description
+
+  belongs_to :creator
+  belongs_to :position
+  #has_one :position
+  has_and_belongs_to_many :tags
+
+  def links
+    {
+        self: api_event_path(object.id),
+        creator: api_creator_path(object.creator.id),
+        position: api_position_path(object.position.id),
+        tag: api_event_tags_path(object.id)
+    }
+  end
+
+end
